e644289b71b086e1f47442bd2ffafc741e5beed0
Karl Poulson <programmingnpc@gmail.com>
2023-10-20 19:16:53 -0600

    Additional Instructions and bug fixes

---
+++ README.txt
@@ -0,0 +1,21 @@
+OVERVIEW:
+    This program is a simple consumer program for Amazon AWS. 
+    It is a command line program that takes two arguments, 
+    the source and destination bucket names. It then copies all
+    the files from the source bucket to the destination bucket.
+    The bucket may be an s3 bucket or a dynamodb table.
+
+
+USAGE:
+    Run the produce to populate bucket:
+        java -jar producer.jar --request-bucket=<bucket-name>
+
+    Run consumer, get requests from destination bucket and write to source bucket:
+        python3 consumer.py -rb <source-bucket> -wb <destination-bucket>
+
+    Run consumer, get requests from destination bucket to dynamodb table:
+        python3 consumer.py -rb <source-bucket> -wb <dynamodb-table>
+
+LOGS:
+    Logs are saved in the logs directory.
+    Logs are track each request, it's type, and it's ID 
--- consumer.py
+++ consumer.py
@@ -1,13 +1,10 @@
 """
-Specs: need to provide cmd line args for: storage strategy, and resources to use
-Example of args: python consumer.py -rb usu-cs5250-blue-requests -dwt widget 
+Simple Consumer program that will read from a bucket, process the requests
+and update the destination bucket/table 
 
-Instructions: 
-in this assignment, you will write a program similar to the Consumer program used in CS5250.
-Specifically, this Consumer program will read objects (Widget Requests) from an S3 bucket
-(namely, Bucket 2) and then process those requests. Each request specifies results in a single
-Widget creation, update, or deletion in either another S3 bucket (Bucket 3) or in a DynamoDB
-table.
+Created by: Karl Poulson
+A02264961
+Oct 20 2023
 """
 import argparse
 import boto3
@@ -16,31 +13,32 @@ import json
 import logging
 import os
 
-# Configure Logging
-logging.basicConfig(filename='logs/consumer.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# Configure logging
+log_format = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(filename='logs/consumer.log', level=logging.INFO, format=log_format)
+console_handler = logging.StreamHandler()  # Add a stream handler to log to the console
+console_handler.setFormatter(logging.Formatter(log_format))
+logging.getLogger().addHandler(console_handler)  # Add the console handler to the root logger
+
 
 
 """
-Opens the .aws file located in the home directory
+Opens the .aws file located in the home directory and gets the keys
 """
 def get_aws_creds():
     # declare dict to return, and get the path of aws credentials file
     aws_creds = {}
     home_dir = os.path.expanduser("~")
     aws_creds_file_path = os.path.join(home_dir, ".aws", "credentials")
-
     # open file and get the keys from the file
     with open(aws_creds_file_path) as creds:
         data = creds.readlines()
-
         aws_access_key_id = data[1].split("=")[1].strip()
         aws_secret_access_key = data[2].split("=")[1].strip()
         aws_session_token = data[3].split("=")[1].strip()
-
         aws_creds["aws_access_key_id"] = aws_access_key_id
         aws_creds["aws_secret_access_key"] = aws_secret_access_key
         aws_creds["aws_session_token"] = aws_session_token
-
     return aws_creds
 
 
@@ -65,7 +63,7 @@ def does_dynamo_table_exist(table_name, region_name):
 
 
 """
-With creds, get the session and return the s3 client
+With creds, get the session and return the s3 client or dynamo client
 """ 
 def get_session(aws_creds: dict, bucket_name: str):
     session = boto3.Session(
@@ -75,19 +73,23 @@ def get_session(aws_creds: dict, bucket_name: str):
         region_name='us-east-1'
     )
     if does_dynamo_table_exist(bucket_name, 'us-east-1'):
-        return session.client('dynamodb'), True 
+        return session.client('dynamodb')
     elif does_s3_bucket_exist(bucket_name, 'us-east-1'):
-        return session.client('s3'), False
+        return session.client('s3')
 
 
-# Downloads the bucket data and decodes it into a dictionary
+"""
+Downloads the bucket data and decodes it into a dictionary
+"""
 def downloadBucket(session, bucket, key):
     requestObject = session.get_object(Bucket=bucket, Key=key)
     data = requestObject['Body'].read().decode('utf-8')
     return json.loads(data)
 
 
-# Processes json data, removes some fields
+"""
+Processes json data, removes some fields
+"""
 def processData(data):
     returnDict = {}
     returnDict['id'] = data['widgetId']
@@ -97,59 +99,79 @@ def processData(data):
     return returnDict
 
 
-# if saving to dynamoDB, other attributes need to be in own col
-def processOtherAttributes(attributes):
+"""
+Break down other attributes into a singular dictionary
+Return dictionary after converted to dynamodb format
+"""
+def processDynamoData(data):
     returnDict = {}
-    for attribute in attributes:
+    for attribute in data["otherAttributes"]:
         returnDict[attribute['name']] = attribute['value']
-    return returnDict
+    data.pop('otherAttributes')
+    for key in returnDict:
+        data[key] = returnDict[key]
+    return convert_dict_to_dynamodb_format(data)
 
 
-# Main loop to run the consumer
-def run(session, sourceBucket, destBucket, dynamoTable=None):
+"""
+Converts a regular dictionary to a dynamodb dictionary
+"""
+def convert_dict_to_dynamodb_format(data_dict):
+    dynamodb_data = {}
+    for key, value in data_dict.items():
+        if isinstance(value, int):
+            dynamodb_data[key] = {'N': str(value)}
+        elif isinstance(value, list):
+            dynamodb_data[key] = {'L': value}
+        else:
+            dynamodb_data[key] = {'S': str(value)}
+    return dynamodb_data
+
+
+"""
+Get request from source bucket and return a queue
+"""
+def getRequestQueue(session, sourceBucket):
     requestQueue = []
+    logging.info("Listing objects in bucket")
+    requests = session.list_objects(Bucket=sourceBucket)
+    if 'Contents' not in requests:
+        pass
+    else: # sort widgets with smallest key first and ad to queue 
+        requestQueue = sorted(requests['Contents'], key=lambda x: x['Key'])
+    return requestQueue
+
+
+"""
+Main loop to run the consumer
+"""
+def run(source_session, sourceBucket, dest_session, destBucket, dynamoTable=None):
+    requestQueue = getRequestQueue(source_session, sourceBucket)
     stop_times = 10 # will retry to populate queue 10 times before stopping program 
 
     while True:
         # if no more requests, check if there are more to process
         if len(requestQueue) == 0:
-            requests = []
-            try:
-                if dynamoTable:
-                    logging.info("Scanning dynamoDB table")
-                    requests = session.scan(TableName=destBucket)
-                    requests = requests['Items']
-                else:
-                    logging.info("Listing objects in bucket")
-                    requests = session.list_objects(Bucket=sourceBucket)
-            except Exception as e:
-                logging.ERROR(f"Error: {e}")
-            if 'Contents' not in requests:
-                pass
-            else: # sort widgets with smallest key first and ad to queue 
-                requestQueue = sorted(requests['Contents'], key=lambda x: x['Key'])
-            time.sleep(0.1)  # Wait for 100ms
-
-        # if no more requests, end program after 10 tries 
-        if len(requestQueue) == 0:
-            logging.info("Done! No more requests to process")
+            logging.info("No requests to process, checking for more")
+            requestQueue = getRequestQueue(source_session, sourceBucket)
             stop_times -= 1
+            time.sleep(0.5)
             if stop_times == 0:
-                print("Stopping program")
-                logging.info("Stopping program\n\n\n")
+                logging.info("Finished processing requests, Stopping")
                 break
-            continue
+            else:
+                continue
 
         # pop the first request from the queue
         request = requestQueue.pop(0)
 
         # Download the request from S3 bucket2 and decode it into a dict
         requestKey = request['Key']
-        logging.info(f"\nDownloading request: {requestKey}")
-        jsonData = downloadBucket(session, sourceBucket, requestKey)
+        logging.info(f"Downloading request: {requestKey}")
+        jsonData = downloadBucket(source_session, sourceBucket, requestKey)
 
         # delete the request from the bucket
-        session.delete_object(Bucket=sourceBucket, Key=request['Key'])
+        source_session.delete_object(Bucket=sourceBucket, Key=request['Key'])
 
         # get the type of request
         requestType = jsonData['type']
@@ -160,11 +182,12 @@ def run(session, sourceBucket, destBucket, dynamoTable=None):
             data = processData(jsonData)
             requestKey = f"widgets/{data['owner']}/{jsonData['requestId']}"
             logging.info(f"Creating a new widget with id: {jsonData['requestId']}")
-            if dynamoTable:
-                data['otherAttributes'] = processOtherAttributes(data['otherAttributes'])
-                session.put_item(TableName=dynamoTable, Item=data)
-            else: 
-                session.put_object(Bucket=destBucket, Key=requestKey, Body=json.dumps(data))
+
+            if dest_session.meta.service_model.service_name == "dynamodb": # save to dynamoDB
+                data = processDynamoData(data)
+                dest_session.put_item(TableName="widgets", Item=data)
+            else: # save to s3
+                dest_session.put_object(Bucket=destBucket, Key=requestKey, Body=json.dumps(data))
 
         elif requestType == 'update':
             logging.info("Updating a widget")
@@ -173,11 +196,25 @@ def run(session, sourceBucket, destBucket, dynamoTable=None):
             logging.info("Deleting a widget")
 
 
-
+"""
+Authenticate the user, get the session, and run the consumer
+Try Catch blocks to catch errors and logs them
+"""
 def run_consumer(sourceBucket='usu-cs5250-quartz-requests', destinationBucket='usu-cs5250-quartz-web'):
-    creds = get_aws_creds()
-    session, dynamoTF = get_session(creds, destinationBucket)
-    run(session, sourceBucket, destinationBucket, dynamoTF)
+    try:
+        creds = get_aws_creds()
+    except Exception as e:
+        logging.error(f"Error, could not find AWS Credentials in ~/.aws/credentials\n {e}")
+    try:
+        source_session = get_session(creds, sourceBucket)
+        dest_session = get_session(creds, destinationBucket)
+    except Exception as e:
+        logging.error(f"Error, could not get session\n {e}")
+    logging.info(f"Running consumer with source bucket: {sourceBucket} and destination bucket: {destinationBucket}")
+    try:
+        run(source_session, sourceBucket, dest_session, destinationBucket)
+    except Exception as e:
+        logging.error(f"Error, could not run consumer\n {e}")
 
 
 if __name__ == '__main__':
